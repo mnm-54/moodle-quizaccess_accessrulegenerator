fde5fe0d02a221dad9bdd00940ba85b87679546c
mnm-54 <shahriarmun54@gmail.com>
2022-07-29 01:48:42 +0600

    initital commit

---
+++ rule.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Implementaton of the accessrulegenerator plugin.
+ *
+ * @package    quizaccess
+ * @subpackage accessrulegenerator
+ * @copyright  2022 munem
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/mod/quiz/accessrule/accessrulebase.php');
+
+class quizaccess_accessrulegenerator extends quiz_access_rule_base
+{
+    /**
+     * Return an appropriately configured instance of this rule, if it is applicable
+     * to the given quiz, otherwise return null.
+     * @param quiz $quizobj information about the quiz in question.
+     * @param int $timenow the time that should be considered as 'now'.
+     * @param bool $canignoretimelimits whether the current user is exempt from
+     *      time limits by the mod/quiz:ignoretimelimits capability.
+     * @return quiz_access_rule_base|null the rule, if applicable, else null.
+     */
+    public static function make(quiz $quizobj, $timenow, $canignoretimelimits)
+    {
+        return null;
+    }
+
+    /**
+     * Whether or not a user should be allowed to start a new attempt at this quiz now.
+     * @param int $numattempts the number of previous attempts this user has made.
+     * @param object $lastattempt information about the user's last completed attempt.
+     * @return string false if access should be allowed, a message explaining the
+     *      reason if access should be prevented.
+     */
+    public function prevent_new_attempt($numprevattempts, $lastattempt)
+    {
+        return false;
+    }
+
+    /**
+     * can override any function from quiz_access_rule_base class in a way needed, 
+     * check the classs defination for help
+     */
+}
